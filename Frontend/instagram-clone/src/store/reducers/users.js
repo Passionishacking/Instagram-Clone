890f3212218f4426e0b1a71e6c930bea7baeb144
Pranshu_Agrawal <113989165+Passionishacking@users.noreply.github.com>
2025-12-14 18:04:24 +0530

    Add files via upload

---
+++ Frontend/instagram-clone/src/store/reducers/users.js
@@ -0,0 +1,73 @@
+import {
+  LOGIN_SUCCESS,
+  LOGOUT_SUCCESS,
+  SIGNUP_SUCCESS,
+  UPDATE_SUCCESS,
+} from "../constants/constants";
+
+const initialState = {
+  token: "",
+  username: "",
+  profile: {
+    id: "",
+    name: "",
+    email: "",
+    bio: "",
+    location: "",
+    profile_pic: "",
+    followers_count: "",
+    following_count: "",
+  },
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SIGNUP_SUCCESS:
+      return {
+        ...state,
+        token: action.payload.token,
+        username: action.payload.username,
+      };
+    case LOGOUT_SUCCESS:
+      return {
+        token: "",
+        username: "",
+        profile: {
+          id: "",
+          name: "",
+          email: "",
+          bio: "",
+          location: "",
+          profile_pic: "",
+          followers_count: "",
+          following_count: "",
+        },
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        token: action.payload.token,
+        username: action.payload.username,
+      };
+    case UPDATE_SUCCESS:
+      return {
+        ...state,
+        profile: {
+          name: action.payload.name || state.profile.name,
+          bio: action.payload.bio || state.profile.bio,
+          location: action.payload.location || state.profile.location,
+          profile_pic: action.payload.profile_pic || state.profile.profile_pic,
+          id: action.payload.id || state.profile.id,
+          email: action.payload.email || state.profile.email,
+          followers_count:
+            action.payload.followers_count || state.profile.followers_count,
+          following_count:
+            action.payload.following_count || state.profile.following_count,
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
